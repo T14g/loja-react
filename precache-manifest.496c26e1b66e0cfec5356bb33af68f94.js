bfed26daddf88b9282f9647662c84ad14ae80255
t14g <ottiagos@gmail.com>
2020-04-29 00:23:12 -0300

    Updates

---
+++ precache-manifest.496c26e1b66e0cfec5356bb33af68f94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e63f33f45772c6aec519990769b157a",
+    "url": "/loja-react/index.html"
+  },
+  {
+    "revision": "97a13170281616d7dc2b",
+    "url": "/loja-react/static/css/main.b8805d82.chunk.css"
+  },
+  {
+    "revision": "7de4d89805564b63a63e",
+    "url": "/loja-react/static/js/2.415c90b2.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/loja-react/static/js/2.415c90b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a13170281616d7dc2b",
+    "url": "/loja-react/static/js/main.55040c91.chunk.js"
+  },
+  {
+    "revision": "360e99cd8fee0895b80b",
+    "url": "/loja-react/static/js/runtime-main.8f9e6f17.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/loja-react/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/loja-react/static/media/shopping-bag.30f07071.svg"
+  }
+]);
