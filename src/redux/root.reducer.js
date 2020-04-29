a4c8f0e10a3f0f9eb4c153b3698748befb17bf89
t14g <ottiagos@gmail.com>
2020-04-28 23:31:48 -0300

    Data persistence

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //window local storage
+
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+
+//Modified version of rootReducer with persistConfig
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root.reducer';
 
 const middlewares = [logger];
 
+//store
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+ //store version with persistence
+const persistor = persistStore(store);
+
+export { store , persistor };
