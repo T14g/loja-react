e9ca571bd22a548fb00f0465ba340e58304309de
t14g <ottiagos@gmail.com>
2020-05-27 09:06:57 -0300

    Fetch Working

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,11 +1,27 @@
 //Listen to every action of a given type
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 
 import ShopActionTypes from './shop.types';
 
+import { firestore, convertCollectionsSnapshotToMap } from  '../../firebase/firebase.utils';
+
+import { fecthCollectionsSuccess, fetchCollectionFailure } from './shop.actions'
+
+//put dispatcher, saga effect for creating actions
+//call chama função
 //generators function must have yeld
+//yield returns { value, done: bool}
+//defer control in this point of execution, permite cancelar, caso demore tomar alguma ação//yield
+ 
 export function* fetchCollectionsAsync() {
-    yield console.log('Im Fired!');
+    try{
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fecthCollectionsSuccess(collectionsMap));
+    }catch (error) {
+        yield put(fetchCollectionFailure(error.message));
+    }    
 }
 
 //Syntax antiga
